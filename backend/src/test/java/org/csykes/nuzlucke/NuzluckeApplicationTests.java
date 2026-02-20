216fee7c99191fff790d5dfa513c6da32f692cbf
Cait <caitlinridgesykes@outlook.com>
2026-02-20 21:47:32 +0000

    + fixed test by incorporating test container + maybe fixed package-lock + application-test.properties + added dependencies to pom

--- backend/src/test/java/org/csykes/nuzlucke/NuzluckeApplicationTests.java
+++ backend/src/test/java/org/csykes/nuzlucke/NuzluckeApplicationTests.java
@@ -1,11 +1,39 @@
 package org.csykes.nuzlucke;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class NuzluckeApplicationTests {
 
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:18-alpine"
+    );
+    
+//    todo: make this more efficient when i actually have tests
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
+    
     @Test
     void contextLoads() {
     }
